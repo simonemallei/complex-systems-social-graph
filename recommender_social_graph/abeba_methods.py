21f731fcfdb6a3b70631cc4781eacb6b0cd586da
simonemallei <simonemallei@gmail.com>
2022-07-01 13:59:29 +0200

    Common interface of ABEBA model (developed with @Okr4m)

---
+++ recommender_social_graph/abeba_methods.py
@@ -0,0 +1,202 @@
+import networkx as nx
+import numpy as np
+from collections import defaultdict
+import random
+'''
+compute_activation performs the activation of the nodes contained in {nodes}.
+The activation by a node is computed by updating his own opinion based
+on his feed and following the ABEBA (Adapted BEBA) model.
+The activated nodes' feed is then cleared since they no longer 
+update their opinion based on that content.
+
+Parameters
+----------
+  G : {networkx.Graph}
+      The graph containing the social network.
+  nodes : {list of object}
+      The list containing activated nodes' IDs (dictionary keys).
+
+Returns
+-------
+  G : {networkx.Graph}
+      The updated graph.
+'''
+def compute_activation(G, nodes):
+  opinions = nx.get_node_attributes(G, 'opinion')
+  all_feeds = nx.get_node_attributes(G, 'feed')
+  beba_beta_list = nx.get_node_attributes(G, 'beba_beta')
+  # Activating update of each node
+  for curr_node in nodes:
+    node_feeds = all_feeds.get(curr_node, [])
+
+    # Computing weight w(i, i)
+    weight_noose = beba_beta_list[curr_node] * opinions[curr_node] * opinions[curr_node] + 1
+
+    # Computing new opinion of curr_node
+    op_num = weight_noose * opinions[curr_node]
+    op_den = weight_noose
+    for feed in node_feeds:
+      # Computing weights w(i, j) where i == curr_node and y(j) == feed
+      weight = beba_beta_list[curr_node] * feed * opinions[curr_node] + 1
+      op_num += weight * feed
+      op_den += weight
+
+    # If the denominator is < 0, the opinion gets polarized and 
+    # the value is set to sgn(opinions[curr_node])
+    if op_den <= 0:
+      opinions[curr_node] = opinions[curr_node] / abs(opinions[curr_node])
+    else:
+      opinions[curr_node] = op_num / op_den
+  
+    # Opinions are capped within [-1, 1] 
+    if opinions[curr_node] < -1:
+      opinions[curr_node] = -1
+    if opinions[curr_node] > 1:
+      opinions[curr_node] = 1
+    all_feeds[curr_node] = []
+  
+  # Updating feed and opinion attributes
+  nx.set_node_attributes(G, all_feeds, 'feed')
+  nx.set_node_attributes(G, opinions, 'opinion')
+
+  return G
+
+'''
+compute_post performs the posting phase of the nodes contained in {nodes}.
+To post an opinion, the node sends to his neighbourhood's feed his 
+opinion with some noise (noise is defined as a normal distribution 
+with mean = 0 and std = {epsilon}).
+By sending to his neighbourhood's feed we mean updating his neighbourhood's
+feed by adding his opinion (with noise) to it. 
+
+Parameters
+----------
+  G : {networkx.Graph}
+      The graph containing the social network.
+  nodes : {list of object}
+      The list containing posting nodes' IDs (dictionary keys).
+  epsilon : {float}
+      The Gaussian noise's standard deviation.
+
+Returns
+-------
+  G : {networkx.Graph}
+      The updated graph.
+'''
+def compute_post(G, nodes, epsilon = 0.0):
+  opinions = nx.get_node_attributes(G, 'opinion')
+  for node_id in nodes:
+    # epsilon defines the standard deviation of the value generated
+    rand_eps = np.random.normal(0, epsilon, 1) 
+    noise_op = rand_eps[0] + opinions[node_id]
+    # Bounded in the range [-1, 1]
+    noise_op = max(noise_op, -1)
+    noise_op = min(noise_op, 1)
+
+    post = [noise_op] # a list with one value
+    past_feed = nx.get_node_attributes(G, 'feed') #get all user feed
+
+    #Spread Opinion
+    all_neig = list(nx.neighbors(G, node_id))   #get all neighbours ID
+
+    
+    post_to_be_added = dict(zip(all_neig,
+                                   [list(post) for _ in range(len(all_neig))] ))
+
+    post_post_to_be_added = {key: past_feed[key] + value 
+                              if key in [*past_feed]
+                              else value
+                              for key, value in post_to_be_added.items()}
+      
+    # Debug print
+    #print('POST ',  post_post_to_be_added)
+    nx.set_node_attributes(G, post_post_to_be_added , name='feed')
+  return G
+
+'''
+simulate_epoch_updated simulates an epoch. It randomly activates a 
+percentage ({percent_updating_nodes}) of graph's nodes and they will
+update their opinion base on their feed.
+Afterwards, a percentage equal to {percentage_posting_nodes} of the activated
+vertices (always sampled randomly) will also be posting nodes, updating 
+their neighbours' feed with the content. 
+The opinion shared by the posting nodes has a noise related
+to the parameter {epsilon}.
+
+Parameters
+----------
+  G : {networkx.Graph}
+      The graph containing the social network.
+  percent_updating_nodes : {int}
+      The percentage of the nodes that will be activated.
+  percent_posting_nodes : {int}
+      The percentage of the activated nodes that will be posting nodes as well.
+  epsilon : {float}
+      The Gaussian noise's standard deviation in the posting phase.
+
+Returns
+-------
+  G : {networkx.Graph}
+      The updated graph.
+'''
+def simulate_epoch_updated(G, percent_updating_nodes, percent_posting_nodes, epsilon = 0.0):
+  # Sampling randomly the activating nodes
+  updating_nodes = int(percent_updating_nodes * len(G.nodes()) / 100)
+  act_nodes = np.random.choice(range(len(G.nodes())), size=updating_nodes, replace=False)
+  # Debug print
+  #print(f"Activated nodes (consuming their feed): {act_nodes}")
+
+  # Executing activation phase: activated nodes will consume their feed
+  G = compute_activation(G, act_nodes)
+
+  # Sampling randomly the posting nodes from activating nodes' list
+  posting_nodes = int(percent_posting_nodes * len(act_nodes) / 100)
+  post_nodes = np.random.choice(act_nodes,size=posting_nodes, replace = False)
+  # Debug print
+  #print(f"Posting nodes: {post_nodes}")
+
+  # Executing posting phase: activated nodes will post in their neighbours' feed
+  G = compute_post(G, post_nodes, epsilon)
+  return G
+
+'''
+apply_initial_feed creates an initial feed for each node.
+A vertex feed will contain {n_post} posts with an opinion that is
+the node's one with some noise (the noise is generated with a normal
+distribution with mean = 0 and a std = {epsilon}).
+
+Parameters
+----------
+  G : {networkx.Graph}
+      The graph containing the social network.
+  n_post : {int}
+      The number of posts created for each node in the initial feed.
+  epsilon : {float}
+      The Gaussian noise's standard deviation.
+
+Returns
+-------
+  G : {networkx.Graph}
+      The updated graph.
+'''
+def apply_initial_feed(G, n_post = 10, epsilon = 0.1):
+  # Casting all the numpy arrays as built-in lists 
+  initial_feed_dict = dict()
+  opinions = nx.get_node_attributes(G, 'opinion')
+
+  for curr_node in G.nodes():
+    # Sampling {n_post} elements from a normal distribution with
+    # - mean = 0.0
+    # - std = epsilon
+    # This values are added with the original opinion in order to have 
+    # a feed that has similar values with the starting opinion
+    feed = np.random.normal(0, epsilon, n_post) + opinions[curr_node]
+    # Bounded in the range [-1, 1].
+    feed = np.maximum(feed, -1)
+    feed = np.minimum(feed, 1)
+    initial_feed_dict[curr_node] = list(feed)
+
+  # Setting these values as feed in the graph
+  nx.set_node_attributes(G, initial_feed_dict, 'feed')
+
+  return G
---
+++ recommender_social_graph/graph_creation.py
@@ -0,0 +1,136 @@
+import networkx as nx
+import numpy as np
+from collections import defaultdict
+import random
+
+def MY_homophilic_ba_graph(N, m,alpha=2, beta=1):
+    """Return homophilic random graph using BA preferential attachment model.
+    A graph of n nodes is grown by attaching new nodes each with m
+    edges that are preferentially attached to existing nodes with high
+    degree. The connections are established by linking probability which 
+    depends on the connectivity of sites and the homophily(similarities).
+    homophily varies ranges from 0 to 1.
+
+    Parameters
+    ----------
+    N : int
+        Number of nodes
+    m : int
+        Number of edges to attach from a new node to existing nodes
+   """
+
+    G = nx.Graph()
+    node_attribute = {}
+    
+
+    for n in range(N):
+        #generate opinion 
+        op = int(random.random()*100)
+        G.add_node(n , opinion = op)
+        node_attribute[n] = op
+
+    #create homophilic distance ### faster to do it outside loop ###
+    dist = defaultdict(int) #distance between nodes
+
+    for n1 in range(N):
+        n1_attr = node_attribute[n1]
+        for n2 in range(N):
+            n2_attr = node_attribute[n2]
+            dist[(n1,n2)] = abs(n1_attr - n2_attr)
+
+
+    target_list = list(range(m))
+    source = m #start with m nodes
+
+    while source < N:
+        targets = _pick_targets(G,source,target_list,dist,m, alpha=alpha ,beta=beta)
+        if targets != set(): #if the node does  find the neighbor
+            G.add_edges_from(zip([source]*m,targets))
+
+        target_list.append(source)  #target list is updated with all the nodes in the graph 
+        source += 1
+
+    return G
+
+def _pick_targets(G,source,target_list,dist,m ,alpha, beta):
+    '''
+    First compute the target_prob which is related to the degree'''
+    target_prob_dict = {}
+    for target in target_list:
+        pow_dist =  (dist[(source,target)]+1)**alpha
+        target_prob = (1/pow_dist)*((G.degree(target)+0.00001)**beta) #formula to compute targer prob, >>Degree better chance
+        target_prob_dict[target] = target_prob
+        
+    prob_sum = sum(target_prob_dict.values())
+
+    targets = set()
+    target_list_copy = target_list.copy()
+    count_looking = 0
+    if prob_sum == 0:
+        return targets #it returns an empty set
+
+    while len(targets) < m:
+        count_looking += 1
+        if count_looking > len(G): # if node fails to find target
+            break
+        rand_num = random.random()
+        cumsum = 0.0
+        for k in target_list_copy:
+            cumsum += float(target_prob_dict[k]) / prob_sum
+            if rand_num < cumsum: 
+                targets.add(k)
+                target_list_copy.remove(k)
+                break
+    return targets
+
+'''
+create_graph creates a graph using the method MY_homophilic_ba_graph,
+then it maps the opinions from the range [0, 100] to [-1, 1] and add
+{edg_feat} features to each edge.
+
+Parameters
+----------
+  n_ag : {int}
+      The numbers of nodes in the graph.
+  beba_beta : {list of float}
+      The list with nodes' beta values.
+  avg_friend: {}
+      The number of average neighbors for each node in the graph.
+  alpha : {float}
+      MY_homophilic_graph's homophily parameter.
+  beta : {float}
+      MY_homophilic_graph's preferential attachment parameter.
+
+Returns
+-------
+  G : {networkx.Graph}
+      Returns the graph obtained.
+'''
+def create_graph(n_ag, beba_beta=[1] , avg_friend=3, hp_alpha=2, hp_beta=1):
+  
+  # checks on beba_beta length
+  if len(beba_beta) != 1 and len(beba_beta) != n_ag:
+    print("WARNING: beba_beta length is not valid. It must be 1 or nodes' number. Default value will be used")
+    beba_beta = [1]
+    beba_beta = [beba_beta[0] for node in range(n_ag)]
+
+  if len(beba_beta) == 1:
+    beba_beta = [beba_beta[0] for node in range(n_ag)]
+
+  # Calls MY_homophilic_ba_graph
+  G = MY_homophilic_ba_graph(n_ag, avg_friend, hp_alpha, hp_beta)
+  
+  # Remapping opinions from [0, 100] to [-1, 1]
+  opinions = nx.get_node_attributes(G, 'opinion')
+  for curr_op in opinions:
+    opinions[curr_op] = (opinions[curr_op] - 50) / 50
+
+  # Setting opinions as node attributes  
+  nx.set_node_attributes(G, opinions, 'opinion')
+
+  # Setting beba_beta as node attributes
+  node_beba_beta_dict = dict(zip(G.nodes(), beba_beta))
+  nx.set_node_attributes(G, node_beba_beta_dict, 'beba_beta')
+
+  return G
+
