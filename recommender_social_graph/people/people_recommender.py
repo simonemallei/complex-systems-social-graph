d6f1cba90d994fe62741a9abed9479623eea29c3
Marco Sgura <m.sgura@campus.unimib.it>
2022-07-22 10:44:28 +0200

    Added the diversity_of_recommendation metric for the People Recommender. Inserted two more strategies for the People Recommender (topology_based still has some problems). Added the gitignore file to exclude temporary files created during Notebook execution and to exclude the folder with the vscode configurations. Added some quick tests in models_comparison. They will then be placed elsewhere or removed in favor of more in-depth testing

--- recommender_social_graph/people/people_recommender.py
+++ recommender_social_graph/people/people_recommender.py
@@ -2,6 +2,54 @@ import networkx as nx
 import numpy as np
 from abeba_methods import compute_activation, compute_post
 
+class RecommendedFriendNotFound(Exception):
+    """Raised when the People Recommender fails to recommend a new friend to a given node"""
+    pass
+
+class SimulateEpochPeopleRecommenderError(Exception):
+    """Raised when an error occurred in the simulate_epoch_people_recommender method"""
+    pass
+
+'''
+x_hop_neighbors is called recursively and returns the nodes after the desired hops.
+Since it receives an undirected graph as input, it excludes the nodes already visited 
+to prevent "going back" in the exploration.
+
+Parameters
+----------
+  G : {networkx.Graph}
+      The graph containing the social network.
+  nodes : {list of object}
+      The list containing nodes' IDs on which to search for nodes at {x_hop}
+  x_hop : {int}     
+      Number of "hops" to do before stopping and returning found nodes
+ previous_nodes : {list of ints}
+      Contains the id of the nodes to be discarded to avoid "going back" 
+      in the exploration of the graph and to return nodes that are not at 
+      the correct number of hops compared to the starting node
+
+Returns
+-------
+  all_new_friends_list_unique : {list of ints}
+      the list the list of node ids that are exactly at x hops according to the starting nodes
+'''
+def x_hop_neighbors(G, nodes, x_hop, previous_nodes = None):
+    all_friends_list = []
+    if previous_nodes is None:
+        previous_nodes = nodes
+    for node in nodes:
+        friends = list(nx.neighbors(G, node))
+        all_friends_list += friends
+    
+    all_new_friends_list = [x for x in all_friends_list if x not in previous_nodes]
+    all_new_friends_list_unique = list(dict.fromkeys(all_new_friends_list))
+    if x_hop == 1:
+        return all_new_friends_list_unique
+    else:
+        previous_nodes += friends
+        return x_hop_neighbors(G, all_new_friends_list_unique, x_hop-1, previous_nodes)
+
+
 '''
 people_recommender performs a people recommender system.
 It performs the recommendation routine on the nodes contained 
@@ -26,17 +74,58 @@ Returns
 def people_recommender(G, nodes, strategy="random"):
     all_nodes = list(G.nodes)
     for node_id in nodes:
+        recommended_friend = None
         neigs = list(nx.neighbors(G, node_id))
-        recommended_friends = [x for x in all_nodes if x not in neigs]
-        recommended_friends.remove(node_id)
+        not_friends = [x for x in all_nodes if x not in neigs]
+        not_friends.remove(node_id)
         if strategy == "random":
-            # recommending a random node not already friend as a new friend
-            recommended_friend = np.random.choice(range(len(recommended_friends)), size=1, replace=False)
-            G.add_edge(node_id, recommended_friend)
-            # deleting a random edge to prevent fully connected graphs
-            discarded_friend = np.random.choice(range(len(neigs)), size=1, replace=False)
-            G.remove_edge(node_id, discarded_friend)
+            nx.set_node_attributes(G, {node_id: not_friends}, 'people_recommended')
+            # recommending a random node not already friend as a new friend. 
+            recommended_friend = np.random.choice(not_friends, size=1, replace=False)
+        elif strategy == 'opinion_diversity':
+            distances_dict = {}
+            opinions = nx.get_node_attributes(G, 'opinion')
+            opinion_subject = opinions[node_id]
+            for key in opinions:
+                if key in not_friends:
+                    abs_distance = abs(opinion_subject - opinions[key])
+                    distances_dict[key] = abs_distance
 
+            # ordered from the largest value to the smallest, therefore from the most discordant to the least discordant node
+            distances_dict_ordered = {k: v for k, v in sorted(distances_dict.items(), key=lambda item: item[1], reverse=True)}
+            # the choice is made between the first 4 keys of the dictionary
+            short_list = list(distances_dict_ordered.keys())[0:4]
+            nx.set_node_attributes(G, {node_id: short_list}, 'people_recommended')
+            recommended_friend = np.random.choice(short_list, size=1, replace=False)
+        # This strategy still doesn't work well
+        # I need to understand what kind of nodes are accepted and then returned by the x_hop_neighbors method
+        elif strategy == 'topology_based':
+            overlapping_dict = {}
+            x_hop_neighbors_list = x_hop_neighbors(G, [node_id], 3)
+            x_hop_not_friends = [x for x in x_hop_neighbors_list if x in not_friends]
+            for not_friend in x_hop_not_friends:
+                not_friend_neigs = list(nx.neighbors(G, not_friend))
+                # the number of mutual friendly nodes is given by the length of the intersection between the two friend lists
+                # note that neither of the two lists can have duplicates
+                number_overlapping_friends = len(set(not_friend_neigs) & set(neigs))
+                overlapping_dict[not_friend] = number_overlapping_friends
+
+            # ordered from the largest value to the smallest, therefore from the most overlapping friends number to the least overlapping friends number node 
+            overlapping_dict_ordered = {k: v for k, v in sorted(overlapping_dict.items(), key=lambda item: item[1], reverse=True)}
+            # the choice is made between the first 4 keys of the dictionary
+            short_list = list(overlapping_dict_ordered.keys())[0:4]
+            nx.set_node_attributes(G, {node_id: short_list}, 'people_recommended')
+            recommended_friend = np.random.choice(short_list, size=1, replace=False)
+
+        if recommended_friend is None:
+            raise RecommendedFriendNotFound
+        else:
+            # note that recommended_friend is a numpy array with 1 element
+            G.add_edge(node_id, recommended_friend[0])
+            # deleting a random edge to prevent fully connected graphs
+            discarded_friend = np.random.choice(neigs, size=1, replace=False)
+            # note that discarded_friend is a numpy array with 1 element
+            G.remove_edge(node_id, discarded_friend[0])
     return G
 
 '''
@@ -84,6 +173,10 @@ def simulate_epoch_people_recommender(G, percent_updating_nodes, percent_posting
   # Executing posting phase: activated nodes will post in their neighbours' feed
   G = compute_post(G, post_nodes, epsilon)
   # Executing content recommender system on activated nodes
-  G = people_recommender(G, post_nodes)
+  try:
+    G = people_recommender(G, post_nodes)
+  except RecommendedFriendNotFound:
+    print("the People Recommender failed to recommend a new friend to a given node")
+    raise SimulateEpochPeopleRecommenderError
 
   return G
---
+++ recommender_social_graph/people/people_recommender_metrics.py
@@ -0,0 +1,42 @@
+import networkx as nx
+from scipy.stats import entropy
+'''
+diversity_of_recommendation returns a metric that represents the entropy of
+the opinions of the people in the Recommended List.
+The entropy metric is computed by putting each opinion in one of
+10 ranges of length 0.2. 
+Parameters
+----------
+  G : {networkx.Graph}
+      The graph containing the feed history to measure.
+  n_buckets : {int} default: 10
+      The number of buckets used to compute the entropy.
+  
+Returns
+-------
+  entropy_dict : {dict}
+      The dictionary containing for each graph's node the entropy
+      of its feed history.
+'''
+def diversity_of_recommendation(G, n_buckets=10):
+    people_recommended_dict = nx.get_node_attributes(G, 'people_recommended')
+    opinions = nx.get_node_attributes(G, 'opinion')
+    entropy_dict = {}
+    for key in people_recommended_dict:
+        # Computing entropy for each not empty feed history
+        people_recommended = people_recommended_dict[key]
+        buckets = [0] * n_buckets
+        for person_recommended in people_recommended:
+            opinion = opinions[person_recommended]
+            # using min in order to not have {n_buckets} as index
+            # (in order to have {n_buckets} buckets)
+            buck_idx = min(n_buckets - 1, int((opinion + 1.0)* n_buckets / 2))
+            # updating counter of {buck_idx}
+            buckets[buck_idx] += 1
+        # useless if the method is used with the people_recommender method because the latter throws an exception when it cannot recommend anyone
+        if len(people_recommended) == 0: 
+            entropy_dict[key] = float('nan')
+        else:
+            buckets = [buck/len(people_recommended) for buck in buckets]
+            entropy_dict[key] = entropy(buckets, base = n_buckets)
+    return entropy_dict
