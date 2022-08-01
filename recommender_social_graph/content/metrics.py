d96f9327001444a443f35a72ace759fb00107fdd
simonemallei <simonemallei@gmail.com>
2022-08-01 16:21:10 +0200

    Added Feed Satisfaction using absolute values

--- recommender_social_graph/content/metrics.py
+++ recommender_social_graph/content/metrics.py
@@ -118,3 +118,70 @@ def feed_satisfaction(G, max_len_history = 20):
             max_sat = (max_x ** cd_alpha) * (((len_feed + 1) - max_x) ** cd_beta) 
             sat_dict[node] = utility / max_sat
     return sat_dict
+
+'''
+feed_satisfaction_weight returns a satisfaction metric that uses the Cobb-Douglas
+utility for each node using cd_beta = 1 / (2 ** (beta[node] + 1)).
+The Cobb-Douglas utility is then normalized using the sum of the absolute values
+of the history feed considered, obtaining the satisfaction value.
+
+Parameters
+----------
+    G : {networkx.Graph}
+        The graph containing the feed history to measure.
+    max_len_history : {int}, default : 20
+        Maximum length of the feed history considered (if we have
+        more than {max_len_history} posts, we'll consider
+        the {max_len_history} newest ones).
+  
+Returns
+-------
+    sat_dict : {dict}
+        The dictionary containing for each graph's node the 
+        satisfaction value of its feed history.
+'''
+def feed_satisfaction_weight(G, max_len_history = 20):
+    feed_history = nx.get_node_attributes(G, 'feed_history')
+    beta = nx.get_node_attributes(G, 'beba_beta')
+    opinion = nx.get_node_attributes(G, 'opinion')
+    sat_dict = {}
+    for node in G.nodes():
+        # Computing normalized Cobb-Douglas Utility for each non-empty feed history
+        curr_history = np.array(feed_history.get(node, []))
+        # multiplying each content's opinion for the node's sign
+        # so if a value in the history is > 0, then it has the same sign of the
+        # node's opinion 
+        sign_node = opinion[node] / abs(opinion[node])
+        curr_history = curr_history * sign_node
+        len_feed = len(curr_history)
+        # if the feed history is empty, the satisfaction has NaN value
+        if len_feed == 0:
+            sat_dict[node] = float('nan')
+        else:
+            # if the history's length is greater than the maximum,
+            # we'll consider the {max_len_history} newest ones
+            if len_feed >= max_len_history:
+                curr_history = curr_history[-max_len_history:]
+                len_feed = max_len_history
+            pos_arr = np.where(curr_history > 0.0, curr_history, 0)
+            neg_arr = np.where(curr_history < 0.0, -curr_history, 0)
+            # sum the absolute values of posts with the same sign
+            # of the node's opinion in {pos_sum}, the opposite ones
+            # in {neg_sum}
+            pos_sum = 1 + np.sum(pos_arr)
+            neg_sum = 1 + np.sum(neg_arr)
+            # Cobb-Douglas beta decreases exponentially by
+            # increasing the node's beta value (we'll be less
+            # satisfied by the opposed content if we have
+            # an higher bias, due to the backfire effect)
+            cd_beta = 1 / (2 ** (beta[node] + 1))
+            cd_alpha = 1 - cd_beta
+            # applying Cobb-Douglas Utility formula
+            utility = ((pos_sum ** cd_alpha) * 
+                       (neg_sum ** cd_beta))
+            # computing maximum possible satisfaction with {pos_sum + neg_sum}
+            # in order to normalize the satisfaction obtained
+            max_x = (pos_sum + neg_sum) * cd_alpha
+            max_sat = (max_x ** cd_alpha) * (((pos_sum + neg_sum) - max_x) ** cd_beta) 
+            sat_dict[node] = utility / max_sat
+    return sat_dict
