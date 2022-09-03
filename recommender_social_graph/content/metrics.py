fb3d5c905c1102e6bb04237e7c5d6c350537c5c5
simonemallei <simonemallei@gmail.com>
2022-09-03 11:28:33 +0200

    Removed NaN values and updated Satisfaction

--- recommender_social_graph/content/metrics.py
+++ recommender_social_graph/content/metrics.py
@@ -31,10 +31,8 @@ def feed_entropy(G, n_buckets=10, max_len_history=30):
         # Computing entropy for each non-empty feed history
         curr_history = feed_history.get(node, [])
         len_feed = len(curr_history)
-        # if the feed history is empty, the entropy has NaN value
-        if len_feed == 0:
-            entropy_dict[node] = float('nan')
-        else:
+        # if the feed history is not empty, we will compute the node's feed entropy
+        if len_feed != 0:
             # if the history's length is greater than the maximum,
             # we'll consider the {max_len_history} newest ones
             if len_feed >= max_len_history:
@@ -79,19 +77,21 @@ Returns
 '''
 def feed_satisfaction(G, max_len_history = 10, sat_alpha = 0.75):
     feed_history = nx.get_node_attributes(G, 'feed_history')
+    feed_length = nx.get_node_attributes(G, 'feed_length')
     beta = nx.get_node_attributes(G, 'beba_beta')
     opinion = nx.get_node_attributes(G, 'opinion')
     sat_dict = nx.get_node_attributes(G, 'feed_satisfaction')
     for node in G.nodes():
         curr_history = np.array(feed_history.get(node, []))
-        len_feed = len(curr_history)
-        # if the feed history is empty, the satisfaction has NaN value
+        len_history = len(curr_history)
+        len_feed = feed_length.get(node, 0)
+        # if the feed history is not empty, we will compute the node's satisfaction
         if len_feed != 0:
-            # if the history's length is greater than the maximum,
-            # we'll consider the {max_len_history} newest ones
-            if len_feed >= max_len_history:
-                curr_history = curr_history[-max_len_history:]
-                len_feed = max_len_history
+            # if we have more than {max_len_history} posts consumed in the last epoch
+            # we will consider them all, otherwise we will consider the last
+            # {max_len_history} posts consumed by the node
+            len_feed = min(len_history, max(max_len_history, len_feed))
+            curr_history = curr_history[-len_feed:]
             # computing weights of each content and then the function is defined as:
             # f(weight) = (weights) / (1 + beta[node] * abs(opinion[node]))
             # afterwards, we compute satisfaction as:
@@ -100,6 +100,7 @@ def feed_satisfaction(G, max_len_history = 10, sat_alpha = 0.75):
             sig_x = (weights) / (1 + beta[node] * abs(opinion[node]))
             satisf = sat_dict.get(node, np.mean(sig_x))
             sat_dict[node] = satisf * sat_alpha + (1 - sat_alpha) * np.mean(sig_x)
+    
     nx.set_node_attributes(G, sat_dict, name = 'feed_satisfaction')
     return sat_dict
     
