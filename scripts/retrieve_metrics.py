9cd8c67825665a784a7ba185d443c4c6f33ba4d0
simonemallei <simonemallei@gmail.com>
2023-03-04 18:49:19 +0100

    fix: fix minor

--- scripts/retrieve_metrics.py
+++ scripts/retrieve_metrics.py
@@ -1,9 +1,42 @@
-from recommender_social_graph.evaluation.utilities import load_json, track_time
-
+import json
 import numpy as np
 import os
 import pandas as pd
+import time
 import warnings
+
+from contextlib import contextmanager
+from typing import Any, Dict, Generator, List, Optional ,Union
+
+
+def load_json(path: str) -> Union[Dict[Any, Any], List[Any]]:
+    """
+    Load a json file as a dictionary or a list.
+
+    :param path: path to the file
+    :return: the object
+    """
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+@contextmanager
+def track_time(msg: Optional[str] = None) -> Generator[None, None, None]:
+    """
+    Tracks the time passed within the context manager.
+
+    :param msg: extra message to write in the output
+    :return: the context manager
+    """
+
+    print(f"Starting {msg}...")
+    start = time.monotonic()
+
+    yield  
+
+    print(f"Ended {msg} in {time.monotonic() - start:.2f} seconds")
+
+
 
 def _get_epoch_metrics(runs, epoch, label):
     """
